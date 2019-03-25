3e5fe712d9579128107c1f7663bfaa4f971c8b45
interglobalmedia <interglobalmedia@gmail.com>
2019-03-24 20:23:32 -0400

    Add post list meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faLinkedinIn, faGithubAlt } from '@fortawesome/free-brands-svg-icons'
 import './post.css'
 import './index.css'
 import Layout from '../components/layout'
+import Author from '../components/author'
 
 const IndexPage = (props) => {
   const postList = props.data.allMarkdownRemark
+  const twitter = `https://twitter.com/letsbsocial1`
+  const github = `https://github.com/interglobalmedia`
+  const linkedin = `https://www.linkedin.com/in/mariacampbell/`
+
   return (
 
     <Layout>
@@ -13,13 +20,31 @@ const IndexPage = (props) => {
         <Link to={node.fields.slug} className="link" >
           <div className="post-list">
             <h1 className="post-list-title">{node.frontmatter.title}</h1>
-            <span>{node.frontmatter.date}</span>
-            <p>{node.excerpt}</p>
+            <p className="post-list-excerpt">{node.excerpt}</p>
+            <div className="post-list-date">
+              <Author /> on {node.frontmatter.date}
+            </div>
+            <ul className="social-media-home">
+              <li>
+                <a href={twitter} target="blank">
+                  <FontAwesomeIcon icon={faTwitter} size='2x' style={{ border: '1px solid rgba(0, 0, 0, 0.8)', borderRadius: '50%', width: '36px', height: '36px', padding: '5px', marginRight: '1rem' }} />
+                </a>
+              </li>
+              <li>
+                <a a href={github} target="blank">
+                  <FontAwesomeIcon icon={faGithubAlt} size='2x' style={{ border: '1px solid rgba(0, 0, 0, 0.8)', borderRadius: '50%', width: '36px', height: '36px', padding: '5px', marginRight: '1rem' }} />
+                </a>
+              </li>
+              <li>
+                <a a href={linkedin} target="blank">
+                  <FontAwesomeIcon icon={faLinkedinIn} size='2x' style={{ border: '1px solid rgba(0, 0, 0, 0.8)', borderRadius: '50%', width: '36px', height: '36px', padding: '5px' }} />
+                </a>
+              </li>
+            </ul>
           </div>
         </Link>
       ))}
-    </Layout >
-
+    </Layout>
   )
 }
 export default IndexPage;
@@ -35,9 +60,20 @@ export const listQuery = graphql`
           frontmatter {
             date(formatString: "MMMM Do YYYY")
             title
+            author
           }
         }
       }
     }
   }
+`
+
+const indexQuery = graphql`
+  query IndexQuery {
+              site {
+            siteMetadata {
+              author
+            }
+          }
+        }
 `
