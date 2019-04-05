c3d95fa3646f9348f4b12c2d9938bf8ff3a1b061
interglobalmedia <interglobalmedia@gmail.com>
2019-04-05 11:19:32 -0400

    MAR-20 #in-progress change path to content

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/pages/blog`,
+        path: `${__dirname}/content`,
         name: 'blog',
       },
     },
