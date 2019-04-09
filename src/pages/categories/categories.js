d8158d8c83a863de06d544f19217c532a964b53e
interglobalmedia <interglobalmedia@gmail.com>
2019-04-09 19:54:44 -0400

    MAR-119 #resolve #time 15m #comment fix data.map()

--- src/pages/categories/categories.js
+++ src/pages/categories/categories.js
@@ -3,41 +3,40 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../../components/Layout/Layout'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolder } from '@fortawesome/free-solid-svg-icons'
-import styled from '@emotion/styled'
 
-function Categories(props) {
+function CategoriesPage(props) {
     const data = props.data.allMarkdownRemark.group
     return (
         <Layout>
             <div>
                 {
-                    data.map((category, index) => {
-                        <Link to={`/categories/${category.fieldValue}`} key={index}>
+                    data.map(category => (
+                        <Link to={`/categories/${category.fieldValue}`}>
                             <FontAwesomeIcon icon={faFolder} style={{ color: '#268bd2' }} />
                             {category.fieldValue} {`(${category.totalCount})`}
                         </Link>
-                    })
+                    ))
                 }
             </div>
         </Layout>
     )
 }
 
-export default Categories
+export default CategoriesPage
 
-export const pageQuery = graphql`
+export const catQuery = graphql`
     query {
-        site {
-            siteMetadata {
-                title
-            }
-        }
-        allMarkdownRemark(
-            limit: 2000) {
-                group(field: frontmatter___categories) {
-                    fieldValue
-                    totalCount
-                }
-            }
+    site {
+      siteMetadata {
+        title
+      }
     }
+    allMarkdownRemark(
+      limit: 2000) {
+      group(field: frontmatter___categories) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
 `
