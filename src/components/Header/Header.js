6e058cb75eb2bce54e361a9ce8d7c48f116a44c2
interglobalmedia <interglobalmedia@gmail.com>
2019-04-09 11:10:45 -0400

    MAR-105 #time 4m #comment remove faInfo import

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { elastic as Menu } from 'react-burger-menu'
 import PropTypes from "prop-types"
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHashtag, faInfo, faHome, faBlog } from '@fortawesome/free-solid-svg-icons'
+import { faHashtag, faHome, faBlog } from '@fortawesome/free-solid-svg-icons'
 import styled from '@emotion/styled'
 import './SideBar.css'
 
