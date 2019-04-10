c7e4d3e7b4221ea07361ea1241c35eb111226581
interglobalmedia <interglobalmedia@gmail.com>
2019-04-10 18:45:43 -0400

    MAR-147 #resolve #time 30m #comment remove search config

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,27 +80,6 @@ module.exports = {
       },
     },
     {
-      resolve: `@gatsby-contrib/gatsby-plugin-elasticlunr-search`,
-      options: {
-        // Fields to index
-        fields: [
-          `title`,
-          `tags`,
-          `categories`
-        ],
-        // How to resolve each field's value for a supported node type
-        resolvers: {
-          // For any node of type MarkdownRemark, list how to resolve the fields' values
-          MarkdownRemark: {
-            title: node => node.frontmatter.title,
-            tags: node => node.frontmatter.tags,
-            tags: node => node.frontmatter.categories,
-            path: node => node.frontmatter.path,
-          },
-        },
-      }
-    },
-    {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
