4753522d2adef44b94e285d35c11d0e2d22dd280
interglobalmedia <interglobalmedia@gmail.com>
2019-04-13 13:08:16 -0400

    MAR-186 #resolve #time 5m #comment add meta gtag remove icons

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 import { Link } from 'gatsby'
-import styled from '@emotion/styled'
 import Layout from '../../components/Layout/Layout'
 import Contact from '../../components/Contact/Contact'
+import { OutboundLink } from 'gatsby-plugin-gtag'
 
 const ContactPage = () => {
     return (
         <Layout>
+            <Helmet>
+                <meta charset="utf-8" />
+                <title>Contact Page</title>
+                <Link rel="canonical" href="https://www.mariadcampbell.com/contact/contact" />
+                <OutboundLink href="https://www.mariadcampbell.com/contact/contact/">Check out Maria D. Campbell's developer notebook contact page!</OutboundLink>
+            </Helmet>
             <Contact />
         </Layout>
     )
