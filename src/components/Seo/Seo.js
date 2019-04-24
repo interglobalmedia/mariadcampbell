d80dd113331b28f46921644283ca9f76b6155c95
interglobalmedia <interglobalmedia@gmail.com>
2019-04-24 13:55:01 -0400

    MAR-191 #in-progress #comment add prettier formatting

--- src/components/Seo/Seo.js
+++ src/components/Seo/Seo.js
@@ -5,101 +5,177 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Helmet} from 'react-helmet'
+import {StaticQuery, graphql} from 'gatsby'
 
-const SEO = ({ title, description, image, pathname, post, keywords, author }) => (
-  <StaticQuery
-    query={query}
-    render={({
-      site: {
-        siteMetadata: {
-          defaultTitle,
-          titleTemplate,
-          defaultDescription,
-          siteUrl,
-          defaultImage,
-          defaultAuthor,
-          defaultKeywords,
-          twitterUsername
-        },
-      }
-    }) => {
-      const seo = {
-        title: title || defaultTitle,
-        description: description || defaultDescription,
-        image: `${image || defaultImage}`,
-        author: author,
-        keywords: keywords || defaultKeywords,
-        siteUrl: `${siteUrl}${pathname || "/"}`,
-      }
-      return (
-        <>
-          <Helmet title={seo.title} titleTemplate={titleTemplate}>
-            <meta name="description" content={seo.description} />
-            <meta name="image" content={seo.image} />
-            {seo.siteUrl && <meta property="og:siteUrl" content={seo.siteUrl} />}
-            {(post ? true : null) && (
-              <meta property="og:type" content="post" />
-            )}
-            {seo.title && <meta property="og:title" content={seo.title} />}
-            {seo.description && (
-              <meta property="og:description" content={seo.description} />
-            )}
-            {seo.image && <meta property="og:image" content={seo.image} />}
-            <meta name="twitter:card" content="summary_large_image" />
-            {twitterUsername && (
-              <meta name="twitter:creator" content={twitterUsername} />
-            )}
-            {seo.title && <meta name="twitter:title" content={seo.title} />}
-            {seo.description && (
-              <meta name="twitter:description" content={seo.description} />
-            )}
-            {seo.image && <meta name="twitter:image" content={seo.image} />}
-          </Helmet>
-        </>
-      )
-    }}
-  />
+const SEO = ({title, description, image, pathname, post, keywords, author}) => (
+    <StaticQuery
+        query={query}
+        render={({
+            site: {
+                siteMetadata: {
+                    defaultTitle,
+                    titleTemplate,
+                    defaultDescription,
+                    siteUrl,
+                    defaultImage,
+                    defaultAuthor,
+                    defaultKeywords,
+                    twitterUsername,
+                },
+            },
+        }) => {
+            const seo = {
+                title: title || defaultTitle,
+                description: description || defaultDescription,
+                image: `${image || defaultImage}`,
+                author: author,
+                keywords: keywords || defaultKeywords,
+                siteUrl: `${siteUrl}${pathname || '/'}`,
+            }
+            return (
+                <>
+                    <Helmet title={seo.title} titleTemplate={titleTemplate}>
+                        <meta name="description" content={seo.description} />
+                        <meta name="image" content={seo.image} />
+                        {seo.siteUrl && (
+                            <meta property="og:siteUrl" content={seo.siteUrl} />
+                        )}
+                        {(post ? true : null) && (
+                            <meta property="og:type" content="post" />
+                        )}
+                        {seo.title && (
+                            <meta property="og:title" content={seo.title} />
+                        )}
+                        {seo.description && (
+                            <meta
+                                property="og:description"
+                                content={seo.description}
+                            />
+                        )}
+                        {seo.image && (
+                            <meta property="og:image" content={seo.image} />
+                        )}
+                        <meta
+                            name="twitter:card"
+                            content="summary_large_image"
+                        />
+                        {twitterUsername && (
+                            <meta
+                                name="twitter:creator"
+                                content={twitterUsername}
+                            />
+                        )}
+                        {seo.title && (
+                            <meta name="twitter:title" content={seo.title} />
+                        )}
+                        {seo.description && (
+                            <meta
+                                name="twitter:description"
+                                content={seo.description}
+                            />
+                        )}
+                        {seo.image && (
+                            <meta name="twitter:image" content={seo.image} />
+                        )}
+                    </Helmet>
+                </>
+            )
+        }}
+    />
 )
 
 SEO.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string,
-  image: PropTypes.string,
-  pathname: PropTypes.string,
-  post: PropTypes.bool,
-  keywords: PropTypes.array,
-  author: PropTypes.string,
-  twitterUsername: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    pathname: PropTypes.string,
+    post: PropTypes.bool,
+    keywords: PropTypes.array,
+    author: PropTypes.string,
+    twitterUsername: PropTypes.string,
 }
 
 SEO.defaultProps = {
-  title: null,
-  description: null,
-  image: null,
-  pathname: null,
-  post: false,
-  keywords: ['web-development', 'react', 'mongodb', 'postgresql', 'gatsbyjs', 'nodejs', 'npm', 'jsx', 'css-in-js', 'styled-components', 'jira', 'atlassian', 'git', 'distributed-version-control', 'github', 'development', 'production', 'continuous-deployment', 'git-integration', 'css3', 'html5', 'audio', 'video', 'full-stack-development', 'front-end-development', 'back-end-development', 'automated-workflows', 'aws', 'netlify', 'gh-pages', 'heroku', 'command-line', 'osx', 'serverless-stack', 'cross-browser-compatibility', 'shadow-dom', 'testing', 'jest-testing', 'html5-canvas', 'webgl', 'linting', 'eslint', 'prettier', 'babel', 'webpack', 'css-modules', 'sass', 'homebrew', 'responsive-design', 'es-6', 'modern-javascript', 'node-security', 'npm-audit-fix'],
-  author: 'Maria D. Campbell'
+    title: null,
+    description: null,
+    image: null,
+    pathname: null,
+    post: false,
+    keywords: [
+        'web-development',
+        'react',
+        'mongodb',
+        'postgresql',
+        'gatsbyjs',
+        'nodejs',
+        'npm',
+        'jsx',
+        'css-in-js',
+        'styled-components',
+        'jira',
+        'atlassian',
+        'git',
+        'distributed-version-control',
+        'github',
+        'development',
+        'production',
+        'continuous-deployment',
+        'git-integration',
+        'css3',
+        'html5',
+        'audio',
+        'video',
+        'full-stack-development',
+        'front-end-development',
+        'back-end-development',
+        'automated-workflows',
+        'aws',
+        'netlify',
+        'gh-pages',
+        'heroku',
+        'command-line',
+        'osx',
+        'serverless-stack',
+        'cross-browser-compatibility',
+        'shadow-dom',
+        'testing',
+        'jest-testing',
+        'html5-canvas',
+        'webgl',
+        'linting',
+        'eslint',
+        'prettier',
+        'babel',
+        'webpack',
+        'css-modules',
+        'sass',
+        'homebrew',
+        'responsive-design',
+        'es-6',
+        'modern-javascript',
+        'node-security',
+        'npm-audit-fix',
+    ],
+    author: 'Maria D. Campbell',
 }
 
 export default SEO
 
 const query = graphql`
-  query SEO {
-    site {
-      siteMetadata {
-        defaultTitle: title
-        titleTemplate
-        defaultDescription: description
-        siteUrl: siteUrl
-        defaultImage: image
-        author
-        twitterUsername
-      }
+    query SEO {
+        site {
+            siteMetadata {
+                defaultTitle: title
+                titleTemplate
+                defaultDescription: description
+                siteUrl: siteUrl
+                defaultImage: image
+                author
+                twitterUsername
+            }
+        }
     }
-  }
 `
