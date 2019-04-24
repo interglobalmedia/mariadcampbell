4ae48f0adea4460b88e2a2a461d34aded880cdd1
interglobalmedia <interglobalmedia@gmail.com>
2019-04-24 13:40:01 -0400

    MAR-191 #in-progress #comment add prettier formatting

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,83 +1,99 @@
 import React from 'react'
-import { StaticQuery, graphql } from "gatsby"
+import {StaticQuery, graphql} from 'gatsby'
 import styled from '@emotion/styled'
-import { rhythm } from '../../utils/typography'
+import {rhythm} from '../../utils/typography'
 import profileSmall from '../../images/profileSmall.png'
-import { SocialStrong } from '../Info/Info'
+import {SocialStrong} from '../Info/Info'
 
 const BioContainerWrapperDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: ${rhythm(2.5)};
+    display: flex;
+    flex-direction: column;
+    margin-bottom: ${rhythm(2.5)};
 `
 
 const BioContainerDiv = styled.div`
-  display: flex; 
-  align-items: flex-start; 
-  justify-content: center;
-  border-top: 1px solid rgba(0,0,0,0.8); 
-  border-bottom: 1px solid rgba(0,0,0,0.8); 
-  padding-top: 1.5rem; 
-  padding-right: 0.25rem;
-  background: rgba(147,173,92,0.4);
-  & a {
-    color: #cb4b16; 
-    box-shadow: none;
-    & :hover {
-      text-decoration: underline;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    border-top: 1px solid rgba(0, 0, 0, 0.8);
+    border-bottom: 1px solid rgba(0, 0, 0, 0.8);
+    padding-top: 1.5rem;
+    padding-right: 0.25rem;
+    background: rgba(147, 173, 92, 0.4);
+    & a {
+        color: #cb4b16;
+        box-shadow: none;
+        & :hover {
+            text-decoration: underline;
+        }
     }
-  }
 `
 
 function Bio() {
-  return (
-    <StaticQuery
-      query={bioQuery}
-      render={data => {
-        const { author, social } = data.site.siteMetadata
-        return (
-          <BioContainerWrapperDiv>
-            <BioContainerDiv>
-              <img
-                src={profileSmall}
-                alt={author}
-                style={{
-                  marginRight: rhythm(1 / 2),
-                  marginLeft: rhythm(1 / 2),
-                  marginBottom: 0,
-                  width: 50,
-                  height: 50,
-                  borderRadius: `50%`,
-                }}
-              />
-              <p>
-                Written by <SocialStrong>{author}</SocialStrong> who lives and works in New York City building useful things. Be sure to follow me on <a href={`https://twitter.com/${social.twitter}`} >Twitter</a>, <a href={`https://github.com/${social.github}`} >Github</a> or <a href={`https://www.linkedin.com/in/${social.linkedin}`}>Linkedin</a>!
-              </p>
-            </BioContainerDiv>
-          </BioContainerWrapperDiv>
-        )
-      }}
-    />
-  )
+    return (
+        <StaticQuery
+            query={graphql`
+                query BioQuery {
+                    site {
+                        siteMetadata {
+                            author
+                            social {
+                                twitter
+                                github
+                                linkedin
+                            }
+                        }
+                    }
+                }
+            `}
+            render={data => {
+                const {author, social} = data.site.siteMetadata
+                return (
+                    <BioContainerWrapperDiv>
+                        <BioContainerDiv>
+                            <img
+                                src={profileSmall}
+                                alt={author}
+                                style={{
+                                    marginRight: rhythm(1 / 2),
+                                    marginLeft: rhythm(1 / 2),
+                                    marginBottom: 0,
+                                    width: 50,
+                                    height: 50,
+                                    borderRadius: `50%`,
+                                }}
+                            />
+                            <p>
+                                Written by <SocialStrong>{author}</SocialStrong>{' '}
+                                who lives and works in New York City building
+                                useful things. Be sure to follow me on{' '}
+                                <a
+                                    href={`https://twitter.com/${
+                                        social.twitter
+                                    }`}
+                                >
+                                    Twitter
+                                </a>
+                                ,{' '}
+                                <a href={`https://github.com/${social.github}`}>
+                                    Github
+                                </a>{' '}
+                                or{' '}
+                                <a
+                                    href={`https://www.linkedin.com/in/${
+                                        social.linkedin
+                                    }`}
+                                >
+                                    Linkedin
+                                </a>
+                                !
+                            </p>
+                        </BioContainerDiv>
+                    </BioContainerWrapperDiv>
+                )
+            }}
+        />
+    )
 }
 
 export default Bio
-
-const bioQuery = graphql`
-query BioQuery {
-    site {
-      siteMetadata {
-        author
-        social {
-          twitter
-        }
-        social {
-          github
-        }
-        social {
-          linkedin
-        }
-      }
-    }
-  }
-`
