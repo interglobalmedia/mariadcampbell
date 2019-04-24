3b1bde8eb10ea79ba9bed41523819cb6a4a0cc27
interglobalmedia <interglobalmedia@gmail.com>
2019-04-24 13:44:00 -0400

    MAR-191 #in-progress #comment add styled exports prettier format

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { CircleArrow as ScrollUpButton } from 'react-scroll-up-button'
-import styled from "@emotion/styled"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDirections, faRss, faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { Link } from 'gatsby'
+import {CircleArrow as ScrollUpButton} from 'react-scroll-up-button'
+import styled from '@emotion/styled'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {
+    faDirections,
+    faRss,
+    faEnvelope,
+} from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'gatsby'
 import InfoPage from '../Info/Info'
 
-const FooterStyle = styled.footer`
+export const FooterStyle = styled.footer`
     text-align: center;
     padding-bottom: 0.5rem;
     letter-spacing: 0.07em;
@@ -18,7 +22,7 @@ const FooterStyle = styled.footer`
     align-items: center;
     justify-content: space-around;
 
-    background: rgba(44,60,160, 1);
+    background: rgba(44, 60, 160, 1);
     & a {
         color: #cb4b16;
         box-shadow: none;
@@ -29,40 +33,62 @@ const FooterStyle = styled.footer`
     }
 `
 
-const CreditWrapperDiv = styled.div`
-    margin-top: 2.25rem; 
+export const CreditWrapperDiv = styled.div`
+    margin-top: 2.25rem;
     color: whitesmoke;
 `
 
 const Footer = () => {
     return (
-        <>
-            <FooterStyle>
-                <div style={{ display: 'flex', fontSize: '1.2rem', letterSpacing: '0.07em' }}>
-                    <Link style={{ marginRight: '1.2rem', color: 'whitesmoke' }} to='/contact/contact'>
-                        <FontAwesomeIcon icon={faEnvelope} style={{ color: 'whitesmoke' }} />
-                        Contact
-                    </Link>
-                    <Link style={{ marginRight: '1.2rem', color: 'whitesmoke' }} to='/sitemap/sitemap'>
-                        <FontAwesomeIcon icon={faDirections} style={{ color: 'whitesmoke' }} />
-                        Sitemap
-                    </Link>
+        <FooterStyle>
+            <div
+                style={{
+                    display: 'flex',
+                    fontSize: '1.2rem',
+                    letterSpacing: '0.07em',
+                }}
+            >
+                <Link
+                    style={{marginRight: '1.2rem', color: 'whitesmoke'}}
+                    to="/contact/contact"
+                >
+                    <FontAwesomeIcon
+                        icon={faEnvelope}
+                        style={{color: 'whitesmoke'}}
+                    />
+                    Contact
+                </Link>
+                <Link
+                    style={{marginRight: '1.2rem', color: 'whitesmoke'}}
+                    to="/sitemap/sitemap"
+                >
+                    <FontAwesomeIcon
+                        icon={faDirections}
+                        style={{color: 'whitesmoke'}}
+                    />
+                    Sitemap
+                </Link>
 
-                    <Link style={{ color: 'whitesmoke' }} to='/rss.xml'>
-                        <FontAwesomeIcon icon={faRss} style={{ color: 'whitesmoke' }} />
-                        RSS
-                    </Link>
+                <Link style={{color: 'whitesmoke'}} to="/rss.xml">
+                    <FontAwesomeIcon
+                        icon={faRss}
+                        style={{color: 'whitesmoke'}}
+                    />
+                    RSS
+                </Link>
+            </div>
+            <InfoPage />
 
-                </div>
-                <InfoPage />
-
-                <ScrollUpButton style={{
+            <ScrollUpButton
+                style={{
                     outline: 'none',
-                    background: 'transparent'
-                }} />
-                <CreditWrapperDiv>© {new Date().getFullYear()} Maria D. Campbell</CreditWrapperDiv>
-            </FooterStyle>
-        </>
+                    background: 'transparent',
+                }}
+            />
+            <CreditWrapperDiv>
+                © {new Date().getFullYear()} Maria D. Campbell
+            </CreditWrapperDiv>
+        </FooterStyle>
     )
 }
 
