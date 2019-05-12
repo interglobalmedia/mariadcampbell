89842d9542451a20277d796f5018bbade72f0250
interglobalmedia <interglobalmedia@gmail.com>
2019-05-11 22:15:16 -0400

    MAR-193 #in-progress #comment add seo graphql remove helmet

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,34 +1,33 @@
 import React from 'react'
-import {Helmet} from 'react-helmet'
-import {Link} from 'gatsby'
+import {graphql} from 'gatsby'
 import Layout from '../../components/Layout/Layout'
 import Contact from '../../components/Contact/Contact'
-import {OutboundLink} from 'gatsby-plugin-gtag'
+import SEO from '../../components/Seo/Seo'
 
-const ContactPage = () => {
+const ContactPage = props => {
+    const {data} = props
+    const siteTitle = data.site.siteMetadata.siteTitle
+    const keywords = data.site.siteMetadata.keywords
     return (
-        <>
-            <Helmet>
-                <meta charset="utf-8" />
-                <meta
-                    name="keywords"
-                    content="web development, react, mongodb, postgresql, gatsbyjs, nodejs, npm, jsx, css in js, styled components, jira, atlassian, git, distributed version control, github, development, production, continuous deployment, git integration, css3, html5, audio, video, full stack development, front end development, back end development, automated workflows, aws, netlify, gh-pages, heroku, command line, osx, serverless stack, cross browser compatibility, shadow dom, testing, jest testing, html5 canvas, webgl, linting, eslint, prettier, babel, webpack, css modules, sass, homebrew, responsive design, es6, modern javascript, node security, npm audit fix"
-                />
-                <title>Contact Page</title>
-                <Link
-                    rel="canonical"
-                    href="https://www.mariadcampbell.com/contact/contact"
-                />
-                <OutboundLink href="https://www.mariadcampbell.com/contact/contact/">
-                    Check out the Maria D. Campbell developer notebook contact
-                    page!
-                </OutboundLink>
-            </Helmet>
-            <Layout>
-                <Contact />
-            </Layout>
-        </>
+        <Layout>
+            <SEO
+                location={props.location}
+                title={siteTitle}
+                keywords={keywords}
+            />
+            <Contact />
+        </Layout>
     )
 }
 
 export default ContactPage
+
+export const contactQuery = graphql`
+    query contactQuery {
+        site {
+            siteMetadata {
+                siteTitle
+            }
+        }
+    }
+`
